e58e1e7cb091b759a927e27c026956b3117f2a63
Trevor Henderson <tfh@mit.edu>
2018-03-03 01:46:04 -0500

    Updated on osx

--- test/basic_test.cpp
+++ test/basic_test.cpp
@@ -5,46 +5,62 @@
 
 #include <boost/filesystem.hpp>
 
-#include "gtest/gtest.h"
 
+void processFiles(boost::filesystem::path pathName, std::string userLibrary) {
 
-void processFiles(boost::filesystem::path pathName) {
-
+  // Make a list of all the samples
   std::vector<AbletonSampleFile> allSamples;
-using namespace boost::filesystem;
+  using namespace boost::filesystem;
 
+  // Iterate over all paths in 
   directory_iterator end_itr;
   for (directory_iterator itr(pathName);
       itr != end_itr;
       itr++) {
+
     if ( is_directory(itr -> status())) {
-      processFiles(itr -> path());
+
+      // If it is a directory, recurse
+      processFiles(itr -> path(), userLibrary);
+
     } else if (itr -> path().extension() == ".alc") {
+
+      // We found a sample file!
       std::string fileName = itr -> path().native();
-      std::cout << allSamples.size() << std::endl;
-      AbletonSampleFile sample(fileName);
-      for (long i = 0; i < allSamples.size(); i++) {
-        //sample.isCompatible(allSamples[i]);
-      }
+      std::cout << "File: " << fileName << std::endl;
+
+      // Check the other samples to see if
+      // this one is similar
+      AbletonSampleFile sample(fileName, userLibrary, true);
+      std::cout << sample.getReferenceFilePath() << std::endl;
+      sample.process();
+
+      int tuning = sample.getAudioSample() -> getTuningCents();
+      double tempo = 60.*(sample.getAudioSample() -> getBeatWithTuning());
+
+      std::cout << tuning << std::endl;
+      std::cout << tempo << std::endl;
+
+      //for (long i = 0; i < allSamples.size(); i++) {
+      //  sample.isCompatible(allSamples[i]);
+      //}
+
+      // Add the sample to the list
+      sample.writeToFile();
       allSamples.push_back(sample);
-      std::cout << "new size: " << allSamples.size() << std::endl;
     }
   }
 }
 
-
-TEST(SamplesTest, FirstTest) {
-  //std::string library = "/Users/tfh/Dropbox (MIT)/UserLibrary/SampleLibrary/Chops";
-  //boost::filesystem::path libraryPath(library);
-  //processFiles(libraryPath);
-  
-  AbletonSampleFile a("../testFile.alc");
-  a.process();
-  std::cout << "tuning cents: " << a.getAudioSample() -> getTuningCents() << std::endl;
-  std::cout << "tempo: " << 60.*(a.getAudioSample() -> getBeatWithTuning()) << std::endl;
-}
-
 int main(int argc, char ** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  // The library file
+  std::string library = "/Users/tfh/Documents/Samples/Binaural";
+  // The user library file
+  std::string userLibrary = "/Users/tfh/Documents/UserLibrary/";
+
+  // Convert it to a boost path
+  boost::filesystem::path libraryPath(library);
+
+  // Process!
+  processFiles(libraryPath, userLibrary);
 }
